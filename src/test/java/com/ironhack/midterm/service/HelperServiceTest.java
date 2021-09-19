7dfe22da888e0c2aaa7abe9a3520f8be87535952
Varvara Nechaeva <85786864+VarvaraNechaeva@users.noreply.github.com>
2021-09-19 21:18:20 +0200

    remove unused imports

--- src/test/java/com/ironhack/midterm/service/HelperServiceTest.java
+++ src/test/java/com/ironhack/midterm/service/HelperServiceTest.java
@@ -1,13 +1,10 @@
 package com.ironhack.midterm.service;
 
-import com.ironhack.midterm.repository.AccountHolderRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.text.ParseException;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class HelperServiceTest {
